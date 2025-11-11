dc84898c3a6df6100cc8902d6fa3b59d084c2d58
MdAsraful56 <work.mdasraful56@gmail.com>
2025-11-11 21:57:45 +0600

    home page design

--- src/app/(commonLayout)/page.tsx
+++ src/app/(commonLayout)/page.tsx
@@ -1,3 +1,17 @@
+import { Hero } from '../../components/modules/Home/Hero';
+import Specialities from '../../components/modules/Home/Specialties';
+import Steps from '../../components/modules/Home/Steps';
+import Testimonials from '../../components/modules/Home/Testimonials';
+import TopRatedDoctors from '../../components/modules/Home/TopRatedDoctors';
+
 export default function Home() {
-    return <div className=''></div>;
+    return (
+        <div className=''>
+            <Hero />
+            <Specialities />
+            <TopRatedDoctors />
+            <Steps />
+            <Testimonials />
+        </div>
+    );
 }
