69849fed2e2f3f390197ede3ff2301ec6f1524e7
MdAsraful56 <work.mdasraful56@gmail.com>
2025-11-14 23:12:59 +0600

    cookies set

--- src/services/auth/loginUser.ts
+++ src/services/auth/loginUser.ts
@@ -1,5 +1,7 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 'use server';
+import { parse } from 'cookie';
+import { cookies } from 'next/headers';
 import { loginValidationZodSchema } from './zod';
 
 export const loginUser = async (
@@ -7,6 +9,8 @@ export const loginUser = async (
     formData: any
 ): Promise<any> => {
     try {
+        let accessTokenObject: null | any = null;
+        let refreshTokenObject: null | any = null;
         const loginData = {
             email: formData.get('email'),
             password: formData.get('password'),
@@ -32,9 +36,59 @@ export const loginUser = async (
             headers: {
                 'Content-Type': 'application/json',
             },
-        }).then((res) => res.json());
+        });
+        const result = await res.json();
 
-        return res;
+        const setCookieHeaders = res.headers.getSetCookie();
+
+        if (setCookieHeaders && setCookieHeaders.length > 0) {
+            setCookieHeaders.forEach((cookie: string) => {
+                const parsedCookie = parse(cookie);
+
+                if (parsedCookie['accessToken']) {
+                    accessTokenObject = parsedCookie;
+                }
+                if (parsedCookie['refreshToken']) {
+                    refreshTokenObject = parsedCookie;
+                }
+            });
+        } else {
+            throw new Error('No Set-Cookie header found');
+        }
+
+        if (!accessTokenObject) {
+            throw new Error('Tokens not found in cookies');
+        }
+
+        if (!refreshTokenObject) {
+            throw new Error('Tokens not found in cookies');
+        }
+
+        const cookieStore = await cookies();
+
+        cookieStore.set('accessToken', accessTokenObject.accessToken, {
+            secure: true,
+            httpOnly: true,
+            maxAge: parseInt(accessTokenObject['Max-Age']) || 1000 * 60 * 60,
+            path: accessTokenObject.Path || '/',
+            sameSite: accessTokenObject['SameSite'] || 'none',
+        });
+
+        cookieStore.set('refreshToken', refreshTokenObject.refreshToken, {
+            secure: true,
+            httpOnly: true,
+            maxAge:
+                parseInt(refreshTokenObject['Max-Age']) ||
+                1000 * 60 * 60 * 24 * 90,
+            path: refreshTokenObject.Path || '/',
+            sameSite: refreshTokenObject['SameSite'] || 'none',
+        });
+
+        if (!res.ok) {
+            return { success: false, error: result.message || 'Login failed' };
+        }
+
+        return result;
     } catch (error) {
         console.log(error);
         return { error: 'Login failed' };
